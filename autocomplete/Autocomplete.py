d9cb4803f7874bac932300848b4713aaeb1ff21a
pab-h <dev.pab.2020@gmail.com>
2023-01-17 11:55:28 -0300

    autocomplete class

---
+++ autocomplete/Autocomplete.py
@@ -0,0 +1,32 @@
+from BaseSearchEngine import BaseSearchEngine
+
+class Autocomplete:
+    def __init__(self, engine: BaseSearchEngine):
+        self.__engine = engine
+
+    @property
+    def engine(self):
+        return self.__engine
+
+    @engine.setter
+    def engine(self, other_engine: BaseSearchEngine):
+        self.__engine = other_engine
+
+    def suggestions_to(self, prefix: str):
+        return self.__engine.search(prefix)
+
+if __name__ == "__main__":
+    from LinearSearchEngine import LinearSearchEngine
+    from Dictionary import Dictionary
+
+    import os 
+
+    filename = os.path.join(os.getcwd(), "dictionary.txt")
+
+    dictionary = Dictionary(filename)
+
+    linearEngine = LinearSearchEngine(dictionary)
+
+    autocomplete = Autocomplete(linearEngine)
+
+    print(autocomplete.suggestions_to("brasi"))
