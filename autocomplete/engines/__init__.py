41eef5c43cbeddb03b808524c1149add5e0311c0
pab-h <dev.pab.2020@gmail.com>
2023-02-16 08:07:32 -0300

    import triesearcengine to all

--- autocomplete/engines/__init__.py
+++ autocomplete/engines/__init__.py
@@ -1,4 +1,5 @@
 from .BaseSearchEngine import BaseSearchEngine
 from .LinearSearchEngine import LinearSearchEngine
+from .TrieSearchEngine import TrieSearchEngine
 
-__all__ = ["BaseSearchEngine", "LinearSearchEngine"]
+__all__ = ["BaseSearchEngine", "LinearSearchEngine", "TrieSearchEngine"]
